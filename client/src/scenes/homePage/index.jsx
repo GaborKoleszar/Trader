b57c8c734f1ad0a9dc78b4734b741deb7c44db4d
Gábor, Koleszár <gabor.koleszar@wup.hu>
2022-12-10 23:16:35 +0100

    Add react navbar components WiP

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import NavBar from "scenes/navbar/index";
+
+const HomePage = () => {
+    return(<Box>
+        <NavBar />
+    </Box>);
+};
+
+export default HomePage
